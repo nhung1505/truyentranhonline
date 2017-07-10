bea8f5287226f22d22e9ba2edf617e1efb244cbe
Nhung <nguyennhungcl1505@gmail.com>
2017-07-10 08:26:09 +0700

    commit

---
+++ js/myjs.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider1').bxSlider({
+        slideWidth: 500,
+        minSlides: 6,
+        maxSlides: 6,
+        slideMargin: 3
+    });
+});/**
+ * Created by nguyen nhung on 7/4/2017.
+ */
